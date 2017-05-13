52736c19db5f424710b6862e0a7939c5589e2664
Aniket <aniket.gadre@sap.com>
2017-05-13 22:08:01 +0800

    Create align endpoint

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 "use strict";
 
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 var app = express();
 var port = process.env.PORT || 5000;
 
 //Define Routes
 //var navigationRouter = require('./src/routes/navigationRoutes.js')();
-//var previewRouter = require('./src/routes/previewRoutes.js')();
+var alignRouter = require('./src/routes/align.js')();
 
 //Middleware
 app.use(express.static('resources'));
@@ -15,9 +17,14 @@ app.use(express.static('resources'));
 app.set('views','./src/views');
 app.set('view engine','ejs');
 
+app.use(bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 //Use Routes
 //app.use('/', navigationRouter);
-//app.use('/preview', previewRouter);
+app.use('/align', alignRouter);
 
 app.listen(port, function(err) {
 	console.log('running server on port ' + port);
---
+++ src/routes/align.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var express = require('express');
+var alignRouter = express.Router();
+
+var pp = require('../controller/planet_positions.js');
+//var pp = new PlanetPositions();
+
+module.exports = function () {
+
+    alignRouter.route('/')
+		.post(function(req, res) {
+			
+            //check for resetFlag
+            if(req.body.reset === "true") {
+                var obj = req.body.obj;
+                var lat = decimalToDegrees(req.body.lat);
+                var lon = decimalToDegrees(req.body.lon);
+
+                pp(obj, lat, lon, function(az,al) {
+                    console.log("Az: " + az + " Al: " + al);
+                    res.cookie('azimuth', az);
+                    res.cookie('altitude', al);
+                    res.cookie('lat', lat);
+                    res.cookie('lon', lon);
+
+                    res.set(200);
+			        res.end();
+                });
+            }
+            else {
+                //read from cookies
+                var lat = req.cookies.lat;
+                var lon = req.cookies.lon;
+                var az = req.cookies.azimuth;
+                var al = req.cookies.altitude;
+
+                console.log('lat:' + lat + ' lon:' + lon + ' az:' + az + ' al:' + al);
+
+                res.set(200);
+                res.end();
+            }
+			
+		});
+
+    return alignRouter;
+}
+
+function decimalToDegrees(latlon) {
+    var intDegree = parseInt(latlon);
+    var minutes = (latlon - intDegree) * 60;
+    var intMinutes = parseInt(minutes);
+    var seconds = (minutes - intMinutes) * 60;
+    var intSecs = parseInt(seconds);
+
+    return intDegree + ' ' + intMinutes + ' ' + intSecs;
+}
